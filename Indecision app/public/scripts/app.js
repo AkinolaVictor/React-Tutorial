af36f1e5cd78bb172b4a80f0c6592950e6fc5fc3
AKINOLA Victor <akinolavictor26@gmail.com>
2019-06-22 03:13:51 +0100

    Add files via upload

---
+++ Indecision app/public/scripts/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+console.log("App.js is running!");
+
+//JSX - JavaScrippt XML
+var app = {
+    title: 'Indecision App',
+    subtitle: "it' a great app"
+};
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'h3',
+        null,
+        app.subtitle
+    )
+);
+var approots = document.getElementById('app');
+ReactDOM.render(template, approots);
+
+var user = {
+    name: 'AKINOLA Victor',
+    age: 23,
+    location: 'Nigeria'
+};
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+var approot = document.getElementById('app2');
+ReactDOM.render(template2, approot);
---
+++ Indecision app/src/app.js
@@ -0,0 +1,33 @@
+'use strict';
+console.log("App.js is running!")
+
+//JSX - JavaScrippt XML
+var app={
+   title: 'Indecision App',
+   subtitle: "it' a great app"
+}
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <h3>{app.subtitle}</h3>
+    </div>
+);
+var approots = document.getElementById('app');
+ReactDOM.render(template, approots);
+
+var user={
+    name: 'AKINOLA Victor',
+    age: 23,
+    location: 'Nigeria'
+}
+
+var template2 = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+var approot = document.getElementById('app2');
+ReactDOM.render(template2, approot);
