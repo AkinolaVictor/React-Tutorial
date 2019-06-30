1fe713921f1eba2b70e8ae141664f6ae7e346b9f
AKINOLA Victor <akinolavictor26@gmail.com>
2019-06-30 04:19:42 +0100

    Add files via upload

---
+++ Indecision app/src/app(add event listeners).js
@@ -0,0 +1,68 @@
+'use strict';
+console.log("App.js is running!")
+
+//JSX - JavaScrippt XML
+
+const approots = document.getElementById('app');
+
+const user={
+    name: 'AKINOLA Victor',
+    age: 23,
+    location: 'Nigeria'
+}
+
+const getLocation = (location) =>{
+    if(location){
+        return <p>Location: {location}</p>
+    }
+}
+
+var template = (
+    <div>
+        <h1>{user.name ? user.name : 'Unknown'}</h1>
+        {(user.age && user.age>=18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+const approot = document.getElementById('app');
+let count = 0;
+const addOne = () => {
+    console.log('addOne');
+};
+const minusOne = () => {
+    console.log('minusOne');
+};
+const reset = () => {
+    console.log('reset');
+};
+
+const template3 = (
+    <div>
+        <h1>Count: {count}</h1>
+        <button onClick = {addOne}>+1</button>
+        <button onClick = {minusOne}>-1</button>
+        <button onClick = {reset}>Reset</button>
+    </div>
+);
+ReactDOM.render(template3, approot);
+
+const app = {
+    title: 'Indecision App',
+    subtitle: 'put your file in the hand of the computer',
+    option: ['one', 'two']
+}
+console.log(app.option.length);
+
+var template2 = (
+    <div>
+        <h1>Idecision App</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{(app.option.length>0) ? 'here are your options:': 'no option'}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+const approot2 = document.getElementById('app2');
+ReactDOM.render(template2, approot2);
---
+++ Indecision app/src/app(use forms).js
@@ -0,0 +1,71 @@
+'use strict';
+console.log("App.js is running!")
+
+//JSX - JavaScrippt XML
+
+const approots = document.getElementById('app');
+
+const user={
+    name: 'AKINOLA Victor',
+    age: 23,
+    location: 'Nigeria'
+}
+
+const getLocation = (location) =>{
+    if(location){
+        return <p>Location: {location}</p>
+    }
+}
+
+var template = (
+    <div>
+        <h1>{user.name ? user.name : 'Unknown'}</h1>
+        {(user.age && user.age>=18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
+
+const app = {
+    title: 'Indecision App',
+    subtitle: 'put your file in the hand of the computer',
+    option: []
+}
+//console.log(app.options.length);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const options = e.target.elements.option.value;
+
+    if (options) {
+        app.option.push(options);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
+};
+
+const removeAll = () =>{
+    app.option = [];
+    renderApp();
+}
+const approot2 = document.getElementById('app2');
+const renderApp = () => {
+    var template2 = (
+        <div>
+            <h1>Idecision App</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{(app.option.length>0) ? 'here are your options:': 'no option'}</p>
+            <p>{app.option.length}</p>
+            <button onClick = {removeAll}>Remove All</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit = {onFormSubmit}>
+                <input type = "text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template2, approot2);
+};
+renderApp();
