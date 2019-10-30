a543274ecc58ead52cba699b7fa112267a062696
AKINOLA Victor <akinolavictor26@gmail.com>
2019-10-30 06:02:14 +0100

    Update app.js

--- Indecision app/public/scripts/app.js
+++ Indecision app/public/scripts/app.js
@@ -1,5 +1,5 @@
-/*This would be updated by babel's Engine */
-//would be continued as soon as possible 
+/*This would be updated by babel's Engine  */
+// it would be continued as soon as possible 
 'use strict';
 
 console.log("App.js is running!");
